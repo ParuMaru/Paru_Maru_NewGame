5241d39d6a07222da3aa90b21c96511a71ae2ef4
ParuMaru <erlkonig.g@gmail.com>
2026-01-07 00:32:14 +0900

    Add files via upload

--- battle_director.js
+++ battle_director.js
@@ -205,7 +205,7 @@ export class BattleDirector {
     }
 
     showSplittingTransform(oldName) {
-        this.ui.addLog(`${oldName}は三体に分裂した！`, "#ff00ff");
+        this.ui.addLog(`${oldName}は三皿に取り分けられた！`, "#ff00ff");
         this.music.playSplited(); 
     }
 
